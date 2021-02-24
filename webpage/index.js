2732dd5358de9bb2909396b868a5fb2e55318170
theVerySharpFlat <aidenramen29@gmail.com>
2021-02-24 16:10:26 -0600

    changed locally

--- webpage/index.js
+++ webpage/index.js
@@ -4,12 +4,11 @@ btn.addEventListener("click", cgi);
 function cgi(){
     getHello_cgi();
     console.log("we got to line 6");
-    getOutput_txt();
 }
 function getHello_cgi(){
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "cgi-bin/hello.cgi", true);
+    xhr.open("GET", "cgi-bin/main.cgi", true);
 
     xhr.onload = function(){
         if(xhr.status == 200){
@@ -21,26 +20,6 @@ function getHello_cgi(){
         else if(xhr.status == 403){
             console.log("looks like we got a 403 for the cgi script. Check your file permissions");
         }
-    }
-
-    xhr.send();
-}
-function getOutput_txt(){
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "cgi-bin/hello.cgi", true);
-
-    xhr.onload = function(){
-        if(xhr.status == 200){
-            console.log(xhr.responseText);
-        }
-        else if(xhr.status == 404){
-            console.log("huh, looks like we couldn't find the output file");
-        }
-        else if(xhr.status == 403){
-            console.log("looks like we got a 403 for the output file. Check your file permissions");
-        }
-        console.log("we got here");
     }
 
     xhr.send();
